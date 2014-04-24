c67e239a5e04d81636ad1c86b865e28572201343
Eduardo Santi <eduardo.senacrs@gmail.com>
2014-04-24 16:48:03 -0300

    Menus terminados

--- Projeto GelattisREG/GelattisREG/src/com/senac/App.java
+++ Projeto GelattisREG/GelattisREG/src/com/senac/App.java
@@ -3,54 +3,32 @@ package com.senac;
 import java.util.Scanner;
 
 public class App {
-	
-	private String usuario;
-	private  String senha;
-	
-	Scanner sc = new Scanner(System.in);
-	Helper help = new Helper();	
 
-	public App() {		
-		
-		System.out.println("--- GelattisRED ---");
-		
-		System.out.println("\nDigite o código do seu cargo:");
-		System.out.println(help.imprimirCargos());
-		
-		String codCargo = sc.next();
-		avaliarCargo(codCargo);
-		
-		solicitarDados();
-		
-		help.avaliarOpCaixa(usuario, senha);
-		
-		
-		
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Help help = new Help();
 
-	}	
-	
-	public static void avaliarCargo(String codCargo){
-		switch (codCargo){
+		System.out.println(help.imprimirMenu());
+		String cmd = sc.next();
+
+		switch (cmd) {
 		case "1":
-			System.out.println("Você é um Operador de Caixa?");
+			System.out.println("\nEntre com o login e senha de Operador !");
 			break;
-			
-		case "2":
-			System.out.println("Você é um Gerente de Piso?");
-			break;
-		
-		default:
-			System.err.println("Ops! Código incorreto");
-		}
-	}
-	
-	public void solicitarDados(){
-		System.out.println("Digite seu usuario");
-		usuario = sc.next();
-		
-		System.out.println("Digite sua senha");
-		senha = sc.next();
-		
-	}
 
+		case "2":
+			System.out.println("\nEntre com o login e senha de Gerente !");
+			break;
+
+		case "9":
+			System.err.println("Programa encerrado !");
+			System.exit(0);
+			break;
+
+		default:
+			System.err.println("Ops, opção inválida !");
+			break;
+		}
+
+	}
 }
---
+++ Projeto GelattisREG/GelattisREG/src/com/senac/Help.java
@@ -0,0 +1,13 @@
+package com.senac;
+
+public class Help {
+	
+	public String imprimirMenu(){
+		return "----- GelattisREG -----" +
+			   "\n1 - Operador de Caixa" +
+			   "\n2 - Gerente de Piso" +
+			   "\n9 - Sair" +
+			   "\nEntre com a opção desejada:";
+	}
+	
+}
