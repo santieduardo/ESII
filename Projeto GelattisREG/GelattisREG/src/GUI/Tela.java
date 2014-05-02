130beb831e29d36cc1abd964839fc2f3db1ca7a8
Eduardo Santi <eduardo.senacrs@gmail.com>
2014-05-01 22:32:41 -0300

    Iniciada a codificação de interface gráfica

--- Projeto GelattisREG/GelattisREG/src/GUI/Tela.java
+++ Projeto GelattisREG/GelattisREG/src/GUI/Tela.java
@@ -1,34 +1,45 @@
+package GUI;
+
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
 public class Tela {
 	
 	JFrame janela;
-	JPanel painelLogin, painelBotoes;
+	JPanel painelLogin, painelBotoes, painelSelecaoFunc;
 	JLabel lblCodigo, lblSenha;
 	JTextField txtCodigo, txtSenha;
 	JButton btnOK, btnCancelar;
+	JRadioButton rdbOperador, rdbGerente;
+	ButtonGroup grupoRDB;
+	private int codigo;
+	private int senha;
 	
 	
 	public Tela(){
 		instanciarElementos();		
 		janela.getContentPane().setLayout(new BorderLayout());
 		
-		setPainelLogin();		
+		setPainelLogin();
+		setOpcaoLogin();
 		setPainelBotoes();
 				
 		setConfigJanela();
 	}
 	
 	private void instanciarElementos(){
-		janela = new JFrame("Insira seus dados");
+		janela = new JFrame("Login");
 		lblCodigo = new JLabel("Código:");
 		lblSenha = new JLabel("Senha:");
 		txtCodigo = new JTextField();
@@ -37,6 +48,10 @@ public class Tela {
 		btnCancelar = new JButton("Cancelar");
 		painelLogin = new JPanel();
 		painelBotoes = new JPanel();
+		painelSelecaoFunc = new JPanel();
+		rdbOperador = new JRadioButton("Operador");
+		rdbGerente = new JRadioButton("Gerente");
+		grupoRDB = new ButtonGroup();
 	}
 	
 	private void setPainelLogin(){
@@ -48,15 +63,43 @@ public class Tela {
 		janela.add(painelLogin, BorderLayout.NORTH);
 	}
 	
+	private void setOpcaoLogin(){
+		grupoRDB.add(rdbOperador);
+		grupoRDB.add(rdbGerente);
+		painelSelecaoFunc.add(rdbOperador);
+		painelSelecaoFunc.add(rdbGerente);
+		janela.add(painelSelecaoFunc, BorderLayout.CENTER);
+	}
+	
 	private void setPainelBotoes(){
 		painelBotoes.setLayout(new FlowLayout());
+		
 		painelBotoes.add(btnOK);
+		btnOK.addActionListener(new ActionListener() {			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				codigo = Integer.parseInt(txtCodigo.getText());
+				senha = Integer.parseInt(txtSenha.getText());
+				//TODO verificar se é operador ou gerente que foi selecionado no radioButton
+				//TODO verificar com os usuarios de teste
+				//TODO criar a classe TratadorBotoes()
+				//TODO lançar e implementar os eventos dos botoes para classe TratadorBotoes()
+			}
+		});
+		
 		painelBotoes.add(btnCancelar);
+		btnCancelar.addActionListener(new ActionListener() {			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		
 		janela.add(painelBotoes, BorderLayout.SOUTH);
 	}
 	
 	private void setConfigJanela(){
-		janela.setSize(200,120);
+		janela.setSize(200,140);
 		janela.setVisible(true);
 		janela.setDefaultCloseOperation(janela.EXIT_ON_CLOSE);
 		janela.setResizable(false);
